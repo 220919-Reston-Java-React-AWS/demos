5cac8e7e69fcbb410f2fc5f0bf1ff58d3239999b
Bach T <bach_tran@outlook.com>
2022-10-03 16:46:04 -0400

    Add docker example for javalin app

---
+++ week3/javalin-docker-example/src/main/java/com/revature/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.revature.controller;
+
+import io.javalin.Javalin;
+
+public class HelloWorldController {
+
+    public void mapEndpoints(Javalin app) {
+
+        app.get("/hello", (ctx) -> {
+            ctx.result("Hello there!");
+        });
+
+    }
+
+}
